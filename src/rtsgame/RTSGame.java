83553fd92c269a9f7a3cbfc9c4315536827d8d0a
Daniel Oliynyk <daniel.oliynyk@gmail.com>
2016-01-06 13:36:11 -0500

    Added background

--- src/rtsgame/RTSGame.java
+++ src/rtsgame/RTSGame.java
@@ -2,9 +2,14 @@ package rtsgame;
 
 import gametools.*;
 import static gametools.Tools.*;
+import java.awt.Color;
+import java.awt.image.BufferedImage;
 
 public class RTSGame extends Game {
     Ship mothership;
+    Group stars;
+    BufferedImage star;
+    int starCooldown;
     
     public static void main(String[] args) {
         initialize(RTSGame.class);
@@ -14,16 +19,35 @@ public class RTSGame extends Game {
     @Override
     protected void window() {
         setTitle("RTS Game");
+        setBackground(new Color(0x0b1037));
         create();
     }
     
     @Override
     protected void setup() {
         mothership = new Ship();
+        star = loadImage("img/star.png");
+        stars = new Group();
+        for (int i = 0; i < 200; i++) stars.add(new Sprite(randomPosition(getArea()), star));
+        starCooldown = 0;
     }
     
     @Override
     protected void run() {
         mothership.draw();
+        starCooldown--;
+        if (starCooldown < 0) {
+            stars.remove(stars.get(0));
+            stars.add(new Sprite(randomPosition(getArea()), star));
+            starCooldown = 3;
+        }
+        
+        for (Sprite dot : stars.getAll()) {
+            if (dot.getX() < -dot.getWidth()) dot.setX(dot.getX() + getWidth());
+            else if (dot.getX() > getWidth()) dot.setX(dot.getX() - getWidth());
+            if (dot.getY() < -dot.getHeight()) dot.setY(dot.getY() + getHeight());
+            else if (dot.getY() > getHeight()) dot.setY(dot.getY() - getHeight());
+        }
+        stars.drawAll();
     }
 }
