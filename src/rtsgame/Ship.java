3cf85b184656bf32aa89cfeeb4a9aceeb95f7cb0
Aryan Andrew <Aryan Andrew@172.17.144.1>
2016-01-06 13:40:01 -0500

    Mother ship circle movement

--- src/rtsgame/Ship.java
+++ src/rtsgame/Ship.java
@@ -5,4 +5,14 @@ import static gametools.Tools.*;
 
 public class Ship extends Sprite {
     
+    public Ship() {
+        super(loadImage("img/mothership.png"));
+    }
+    
+    @Override
+    protected void update() {
+        if (isWithin(Game.mousePosition())) {
+            Game.painter().drawOval((int) x, (int) y, 300, 300);
+        }
+    }
 }
